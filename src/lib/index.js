fb662fc9b4660ae9eef6dc4a189c096b2e1cfda4
Dominique Klevinghaus <79855242+Klekum@users.noreply.github.com>
2022-01-12 10:58:51 +0100

    Create index.js

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import CommonNavbarGrey from "./components/CommonNavbarGrey";
+import CommonNavbar from "./components/CommonNavbar";
+import CommonFooter from "./components/CommonFooter";
+import CommonCookieNotice from "./components/CommonCookieNotice";
+export { CommonNavbarGrey, CommonNavbar, CommonFooter, CommonCookieNotice };
