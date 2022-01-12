d914cef881186000f0e0f425280f19474f483739
Dominique Klevinghaus <79855242+Klekum@users.noreply.github.com>
2022-01-12 11:13:15 +0100

    Remove jsx

--- jsx/CommonNavbarGrey.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import 'bootstrap/dist/css/bootstrap.min.css';
-interface PropDefs {
-    children?: string;
-}
-declare function CommonNavbarGrey({ children }: PropDefs): JSX.Element;
-export default CommonNavbarGrey;
