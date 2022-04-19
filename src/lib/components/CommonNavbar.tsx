6dbb3be4b8b39a0887f71a6be408609b20edfa88
Dominique Klevinghaus <79855242+Klekum@users.noreply.github.com>
2022-04-19 14:18:01 +0200

    Add link to logo

--- src/lib/components/CommonNavbar.tsx
+++ src/lib/components/CommonNavbar.tsx
@@ -8,14 +8,17 @@ import NavbarBrand from 'react-bootstrap/NavbarBrand';
 interface PropDefs {
   logo?: string,
   children?: object[],
+  link?: string,
 }
 
-function CommonNavbar({ logo, children }:PropDefs) {
+function CommonNavbar({ logo, children, link }:PropDefs) {
   return (
     <Navbar expand="lg" sticky='top'>
       <Container>
         <NavbarBrand>
-          <Image src={logo} className="logo"></Image>
+          <a href={link}>
+            <Image src={logo} className="logo"></Image>
+          </a>
         </NavbarBrand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
@@ -27,7 +30,8 @@ function CommonNavbar({ logo, children }:PropDefs) {
 }
 
 CommonNavbar.defaultProps = {
-  logo: 'https://cdn0.scrvt.com/4d3e519fe5939342b95c7312343779ef/c736dfe931dfb22d/0e6f79ca70bd/Logo-klinikum.svg'
+  logo: 'https://cdn0.scrvt.com/4d3e519fe5939342b95c7312343779ef/c736dfe931dfb22d/0e6f79ca70bd/Logo-klinikum.svg',
+  link: '/'
 }
 
 export default CommonNavbar;
